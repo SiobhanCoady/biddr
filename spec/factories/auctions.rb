059a6f2a991753de35db7707fbc05e814067265c
Siobhan Coady <siobhan.coady@gmail.com>
2017-05-15 09:46:22 -0700

    Add auction, bid, and user models and controllers

---
+++ spec/factories/auctions.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :auction do
+    title "MyString"
+    details ""
+    ends_on ""
+    reserve_price 1
+  end
+end
---
+++ spec/factories/bids.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :bid do
+    amount 1
+    auction nil
+    user nil
+  end
+end
