481a5fe8434222c1122b235a4c509ab479d158a8
Siobhan Coady <siobhan.coady@gmail.com>
2017-05-15 10:34:12 -0700

    Implement basic CRUD for auctions

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,2 +1,36 @@
 class AuctionsController < ApplicationController
+  before_action :find_auction, only: [:show, :edit]
+
+  def index
+    @auctions = Auction.all
+  end
+
+  def new
+    @auction = Auction.new
+  end
+
+  def create
+    @auction = Auction.new auction_params
+    # @auction.user = current_user
+    if @auction.save
+      flash[:notice] = 'Auction created!'
+      redirect_to auction_path(@auction)
+    else
+      flash.now[:alert] = 'Please fix errors below'
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+    def find_auction
+      @auction = Auction.find params[:id]
+    end
+
+    def auction_params
+    params.require(:auction).permit([:title, :details, :ends_on, :reserve_price])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+25.times do
+  Auction.create  title: Faker::Commerce.product_name,
+                  details: Faker::Hipster.paragraph,
+                  ends_on: Faker::Date.forward(90),
+                  reserve_price: rand(500)
+end
+
+puts "#{Auction.count} auctions created!"
